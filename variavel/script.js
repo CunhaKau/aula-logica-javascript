bcd5c62c5674c1ecd419a86f7125c4a9712769be
ANA CLAUDIA SOUZA CUNHA <ana.cscunha1@senacsp.edu.br>
2025-06-09 16:54:56 -0300

    logica

---
+++ variavel/script.js
@@ -0,0 +1,30 @@
+//comentário em uma linha
+
+/* comentário
+
+em várias 
+
+linhas */
+
+console.log(2);
+
+console.log(typeof 2.5);
+
+//declarando constantes
+const meunumero = 10;
+console.log(meunumero);
+
+//declarando variáveis
+let novonumero = 100;
+novonumero = 150;
+console.log(novonumero);
+
+//strings
+console.log("Olá Mundo");
+
+//contagem de string
+const nome = "Ana Claudia";
+console.log(nome.length);
+
+//Pular uma linha na string
+console.log("Pular uma linha para mostrar o resultado \nR$125,00");
